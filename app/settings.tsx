e0d855e6f40563b9bd65f6e274a97acab17f22bb
MrPotatoXx <salinasgiovanni25@gmail.com>
2025-07-15 11:51:49 -0400

    feat: update version to 1.0.1

--- app/settings.tsx
+++ app/settings.tsx
@@ -169,7 +169,7 @@ export default function MoreScreen() {
         </View>
 
         <View style={styles.footer}>
-          <Text style={styles.versionText}>Versión 1.0.0</Text>
+          <Text style={styles.versionText}>Versión 1.0.1</Text>
         </View>
       </ScrollView>
       
