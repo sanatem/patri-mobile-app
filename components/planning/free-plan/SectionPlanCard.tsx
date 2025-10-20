c0b48b49dbbf1e9e8eb42fc42563908db303c5f0
RomminaC <rommina.caceres@gmail.com>
2025-10-20 12:47:27 -0300

    fix: update text color in SectionPlanCard for improved readability

--- components/planning/free-plan/SectionPlanCard.tsx
+++ components/planning/free-plan/SectionPlanCard.tsx
@@ -73,7 +73,7 @@ const SectionPlanCard: React.FC<SectionPlanCardProps> = ({
       <View className="flex-row justify-between items-start mb-4">
         <View className="flex-row items-center flex-1">
           {renderIcon()}
-          <Text className="text-lg font-medium" style={{ color: Colors.primary[700] }}>
+          <Text className="text-lg font-medium" style={{ color: Colors.primary[600] }}>
             {title}
           </Text>
         </View>
@@ -96,7 +96,7 @@ const SectionPlanCard: React.FC<SectionPlanCardProps> = ({
       <View className="w-full mb-4" style={{ backgroundColor: Colors.gray[100], height: 0.5 }} />
       {price && (
         <View className="flex-row items-center justify-between mb-4">
-          <Text className="text-lg font-medium" style={{ color: Colors.primary[700] }}>
+          <Text className="text-lg font-medium" style={{ color: Colors.primary[600] }}>
             {price}
           </Text>
           {duration && (
@@ -115,7 +115,7 @@ const SectionPlanCard: React.FC<SectionPlanCardProps> = ({
         <View className="flex-row items-start justify-between mb-4">
           {minPrice && (
             <View className="flex-1">
-              <Text className="text-lg font-medium" style={{ color: Colors.primary[700] }}>
+              <Text className="text-lg font-medium" style={{ color: Colors.primary[600] }}>
                 {minPrice}
               </Text>
               <Text className="text-xs" style={{ color: Colors.gray[500] }}>
