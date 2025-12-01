a2fea112c4d4a7dffbf47eee267224e80bd9b006
RomminaC <rommina.caceres@gmail.com>
2025-12-01 17:30:01 -0300

    feat: add getBudgetInstances function for fetching budget instances

--- services/budget/budget-instances/index.ts
+++ services/budget/budget-instances/index.ts
@@ -1,6 +1,11 @@
 export { getBudgetInstancesCurrent } from './get-budget-instances-current';
 export { getBudgetInstancesHistory } from './get-budget-instances-history';
+export { getBudgetInstances } from './get-budget-instances';
 export { getBudgetInstanceById } from './get-budget-instance-by-id';
+export type {
+  GetBudgetInstancesParams,
+  GetBudgetInstancesResponse
+} from './get-budget-instances';
 export type {
   BudgetInstanceByIdResponse,
   BudgetInstanceTransaction,
